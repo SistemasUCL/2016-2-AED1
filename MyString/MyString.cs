43003d159d1128bc5e753d5f6193e41dc128d588
andre <andre@ULTRAARS2>
2016-10-27 21:06:23 -0200

    Add project files.

---
+++ MyString/MyString.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyString
+{
+    public class MyString
+    {
+        private string minha_string;
+
+        public MyString() {
+            minha_string = string.Empty;
+        }
+
+        public MyString(string s) {
+            minha_string = s;
+        }
+
+        public string GetString() {
+            return minha_string;
+        }
+
+        public void SetString(string s) {
+            minha_string = s;
+        }
+
+        public bool Digito() {
+            for(int p=0; p<minha_string.Length; p++) {
+                char c = minha_string[p];
+                if (c != '0' && c != '1' && c != '2' && c != '3' &&
+                    c != '4' && c != '5' && c != '6' && c != '7' &&
+                    c != '8' && c != '9') {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
---
+++ MyString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyString
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            MyString palavra = new MyString("12341234");
+
+            if(palavra.Digito()) {
+                Console.WriteLine("{0} - Apenas dígitos!", palavra.GetString());
+            }
+            else {
+                Console.WriteLine("{0} - Não apenas dígitos!", palavra.GetString());
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
